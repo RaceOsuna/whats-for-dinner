4704585232a894fe39b73e5b0689d0cf56baba29
Race Osuna <raceosuna@gmail.com>
2023-02-18 14:43:36 -0700

    Feat: hide cooking pot/show suggested food

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 //variables
 
-// var entireMeal = document.getElementById("entire-meal")
-// var pot = document.querySelector(".pot")
-
-//var body = document.querySelectorAll('body')
-
+var entireMeal = document.getElementById("entire-meal")
+var pot = document.querySelector(".pot")
+var letsCookButton = document.querySelector(".lets-cook")
+var foodItem = document.querySelector("#food-item")
+var suggest = document.querySelector("#suggest")
 //event listeners
+
+letsCookButton.addEventListener("click", showFoodItem)
 
 //window.addEventListener('load', loadPage)
 
 //functions
+
+function showFoodItem(event) {
+  event.preventDefault()
+  pot.classList.add("hidden")
+  suggest.classList.remove("hidden")
+  foodItem.classList.remove("hidden")
+}
