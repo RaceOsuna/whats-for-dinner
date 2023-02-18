d75ff74b9aa8ba1d8961c92f49efc78209c56f41
Race Osuna <raceosuna@gmail.com>
2023-02-18 13:35:32 -0700

    edit to main

---
+++ main.js
@@ -0,0 +1,12 @@
+//variables
+
+// var entireMeal = document.getElementById("entire-meal")
+// var pot = document.querySelector(".pot")
+
+//var body = document.querySelectorAll('body')
+
+//event listeners
+
+//window.addEventListener('load', loadPage)
+
+//functions
